f1d5457b46eb7fb261b622d286795a965fcc4ea8
Oleksandr Hordiienko <oleksandrhordiienko@mbp-oleksandr.lan>
2018-02-06 11:46:30 +0200

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Oleksandr Hordiienko on 06.02.18.
+//  Copyright © 2018 Oleksandr Hordiienko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
